1d448c98925a8f17e20b66c17e2a137e9d90b3a9
LenaVi <lenavi@user.com>
2019-12-12 23:35:28 +0300

    создание конфликтов

---
+++ Sync/SyncLib/Conf/ExistDirectoryConflict.cs
@@ -0,0 +1,16 @@
+﻿namespace SyncLib
+{
+    public class ExistDirectoryConflict : IConflict
+    {
+        public string DirectoryPath;
+
+        public ExistDirectoryConflict(string path)
+        {
+            DirectoryPath = path;
+        }
+        public void Accept(IVisitor visitor)
+        {
+            visitor.Visit(this);
+        }
+    }
+}
---
+++ Sync/SyncLib/Conf/ExistFileConflict.cs
@@ -0,0 +1,17 @@
+﻿namespace SyncLib
+{
+    public class ExistFileConflict : IConflict
+    {
+        public string FilePath;
+
+        public ExistFileConflict(string path)
+        {
+            FilePath = path;
+        }
+
+        public void Accept(IVisitor visitor)
+        {
+            visitor.Visit(this);
+        }
+    }
+}
