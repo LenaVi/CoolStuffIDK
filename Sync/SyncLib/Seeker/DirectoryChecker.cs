41fbb97b705578c6ad63308b6e5d8fef25b4f613
LenaVi <lenavi@user.com>
2019-12-12 23:42:43 +0300

    чекеры файлов

---
+++ Sync/SyncLib/Seeker/DirectoryChecker.cs
@@ -0,0 +1,12 @@
+﻿namespace SyncLib
+{
+    internal class DirectoryChecker
+    {
+        private string masterPath;
+
+        public DirectoryChecker(string masterPath)
+        {
+            this.masterPath = masterPath;
+        }
+    }
+}
---
+++ Sync/SyncLib/Seeker/FileChecker.cs
@@ -0,0 +1,14 @@
+﻿namespace SyncLib
+{
+    internal class FileChecker
+    {
+        private string slavePath;
+        private string masterPath;
+
+        public FileChecker(string slavePath, string masterPath)
+        {
+            this.slavePath = slavePath;
+            this.masterPath = masterPath;
+        }
+    }
+}
