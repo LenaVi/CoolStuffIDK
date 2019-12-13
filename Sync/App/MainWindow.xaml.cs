2d632a797b6de46a4b638b3464e41e4a6947dd17
Zhuzhuna <56105906+Zhuzhuna@users.noreply.github.com>
2019-12-13 20:29:24 +0300

    Add files via upload

--- Sync/App/MainWindow.xaml.cs
+++ Sync/App/MainWindow.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
+using System.Windows.Forms;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -32,26 +32,62 @@ namespace App
 
         private BaseSeeker conflictSeeker;
         private  ILogger logger;
+
+        private void Master_Click(object sender, RoutedEventArgs e)
+        {
+            FolderBrowserDialog dlg = new FolderBrowserDialog();
+
+            if (dlg.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                if (dlg.SelectedPath == null)
+                    System.Windows.Forms.MessageBox.Show("Вы что-то ничего не выбрали");
+                else master=dlg.SelectedPath;
+            }
+        }
+
+        private void Slave_Click(object sender, RoutedEventArgs e)
+        {
+            FolderBrowserDialog dlg = new FolderBrowserDialog();
+
+            if (dlg.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                if (dlg.SelectedPath == null)
+                    System.Windows.Forms.MessageBox.Show("Вы что-то ничего не выбрали");
+                else slave = dlg.SelectedPath;
+            }
+        }
+
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            master = @"C:\Users\stalk\Documents\GitHub\Students\CoolStuffIDK\Sync\App\bin\Debug\master";
-            slave = @"C:\Users\stalk\Documents\GitHub\Students\CoolStuffIDK\Sync\App\bin\Debug\slave";
-            noDeleteOption = true;
-            logger = LoggerFactory.GetLogger(LoggerType.Verbose, txt);
-            conflictSeeker = ConflictSeekerFactory.GetConflictSeeker(noDeleteOption, master, slave);
-
-            conflictSeeker.GetMasterConflict().ForEach(x =>
+            if ((NoLog.IsChecked == true || Summary.IsChecked == true || Verbose.IsChecked == true) &&
+                (NoDeleteOption.IsChecked == true || DeleteOption.IsChecked == true))
             {
-                x.Accept(new ResolverVisitor());
-                x.Accept(logger);
-            });
+                if (NoLog.IsChecked == true)
+                { logger = LoggerFactory.GetLogger(LoggerType.Silent, txt); }
+                if (Summary.IsChecked == true)
+                { logger = LoggerFactory.GetLogger(LoggerType.Summary, txt); }
+                else
+                { logger = LoggerFactory.GetLogger(LoggerType.Verbose, txt); }
+                if (NoDeleteOption.IsChecked == true)
+                { noDeleteOption = true; }
+                else { noDeleteOption = false; }
+                conflictSeeker = ConflictSeekerFactory.GetConflictSeeker(noDeleteOption, master, slave);
 
-            conflictSeeker.GetSlaveConflicts().ForEach(x => {
-                x.Accept(new ResolverVisitor());
-                x.Accept(logger);
-            });
+                conflictSeeker.GetMasterConflict().ForEach(x =>
+                {
+                    x.Accept(new ResolverVisitor());
+                    x.Accept(logger);
+                });
 
-            logger.Log();
+                conflictSeeker.GetSlaveConflicts().ForEach(x => {
+                    x.Accept(new ResolverVisitor());
+                    x.Accept(logger);
+                });
+
+                logger.Log();
+            }
+            else System.Windows.Forms.MessageBox.Show("Вы не все выбрали");
         }
+
     }
 }
