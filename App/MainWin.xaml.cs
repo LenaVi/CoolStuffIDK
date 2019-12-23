e88728a92a68a727668ae3118ecf098da4a32e14
LenaVi <linaisvi@gmail.com>
2019-12-24 00:37:34 +0300

    загрузка и выгрузка сценариев

--- App/MainWin.xaml.cs
+++ App/MainWin.xaml.cs
@@ -8,6 +8,7 @@ using System.Xml.Serialization;
 using System.Windows.Forms;
 using System.IO;
 using System;
+using System.Collections.Generic;
 
 namespace App
 {
@@ -16,16 +17,17 @@ namespace App
     /// </summary>
     public partial class MainWin : Window
     {
-
-
         public MainWin()
         {
             InitializeComponent();
+
+            _data = new List<PairMasterSlave>();
         }
 
         private ILogger logger;
         private BaseSeeker conflictSeeker;
         private PairMasterSlave pair;
+        private List<PairMasterSlave> _data;
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
@@ -35,11 +37,10 @@ namespace App
             {
                 bool isValid = true;
 
-                foreach (var item in data.Items)
+                foreach (var item in _data)
                 {
-                    PairMasterSlave pair = (PairMasterSlave)item;
-                    if (pair.Slave.Equals(form.CurrentValue.Slave) || pair.Master.Equals(form.CurrentValue.Slave) ||
-                        pair.Slave.Equals(form.CurrentValue.Master))
+                    if (item.Slave.Equals(form.CurrentValue.Slave) || item.Master.Equals(form.CurrentValue.Slave) ||
+                        item.Slave.Equals(form.CurrentValue.Master))
                     {
                         isValid = false;
 
@@ -48,7 +49,10 @@ namespace App
                 }
 
                 if (isValid)
+                {
                     data.Items.Add(form.CurrentValue);
+                    _data.Add(form.CurrentValue);
+                }
                 else System.Windows.Forms.MessageBox.Show("К сожалению, выбранная вами подчиненная папка уже занята");
             }
 
@@ -57,14 +61,18 @@ namespace App
         private void MenuItem_Click(object sender, RoutedEventArgs e)
         {
             if (data.SelectedIndex != -1)
-                data.Items.RemoveAt(data.SelectedIndex);
+            {
+                int index = data.SelectedIndex;
+
+                data.Items.RemoveAt(index);
+                _data.RemoveAt(index);
+            }
         }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
-            foreach (var item in data.Items)
+            foreach (var pair in _data)
             {
-                pair = (PairMasterSlave)item;
                 logger = LoggerFactory.GetLogger(pair.TypeLogger, log);
 
                 conflictSeeker = ConflictSeekerFactory.GetConflictSeeker(pair.NoDelete, pair.Master, pair.Slave);
@@ -79,9 +87,8 @@ namespace App
                 logger.Log();
             }
 
-            foreach (var item in data.Items)
+            foreach (var pair in _data)
             {
-                pair = (PairMasterSlave)item;
                 logger = LoggerFactory.GetLogger(pair.TypeLogger, log);
 
                 conflictSeeker = ConflictSeekerFactory.GetConflictSeeker(pair.NoDelete, pair.Master, pair.Slave);
@@ -108,6 +115,9 @@ namespace App
                     int index = data.SelectedIndex;
                     data.Items.Insert(index, form.CurrentValue);
                     data.Items.RemoveAt(index + 1);
+
+                    _data.Insert(index, form.CurrentValue);
+                    _data.RemoveAt(index + 1);
                 }
             }
             else System.Windows.MessageBox.Show("Вы не выбрали, что хотите изменить");
@@ -126,7 +136,9 @@ namespace App
         private void Load_Click(object sender, RoutedEventArgs e)
         {
             OpenFileDialog dlg = new OpenFileDialog();
+
             dlg.Filter = "Text files(*.xml)|*.xml|All files(*.*)|*.xml";
+
             if (dlg.ShowDialog() == System.Windows.Forms.DialogResult.OK)
             {
                 if (dlg.FileName == null)
@@ -156,6 +168,47 @@ namespace App
                 xmlFormat.Serialize(fStream, log.Text);
             }
             System.Windows.Forms.MessageBox.Show("Сохранено в " + fileName);
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            XmlSerializer xmlFormat = new XmlSerializer(typeof(List<PairMasterSlave>));
+
+            string fileName = string.Format("scenario{0}", DateTime.Now.Date.Day);
+
+            using (Stream fStream = new FileStream(fileName + ".xml",
+              FileMode.Create, FileAccess.Write, FileShare.None))
+            {
+                xmlFormat.Serialize(fStream, _data);
+            }
+            System.Windows.Forms.MessageBox.Show("Сохранено в " + fileName);
+
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+
+            dlg.Filter = "Text files(*.xml)|*.xml|All files(*.*)|*.xml";
+
+            if (dlg.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                if (dlg.FileName == null)
+                    System.Windows.MessageBox.Show("Вы что-то ничего не выбрали");
+                else
+                {
+                    XmlSerializer xmlFormat = new XmlSerializer(typeof(List<PairMasterSlave>));
+
+                    using (Stream fStream = File.OpenRead(dlg.FileName))
+                    {
+                        _data = (List<PairMasterSlave>)xmlFormat.Deserialize(fStream);
+                    }
+
+                    data.Items.Clear();
+
+                    _data.ForEach(x => data.Items.Add(x));
+                }
+            }
         }
     }
 }
