c065d05bfcd5cb6077fbfd94ac6b3af7a16ab7c9
Zhuzhuna <56105906+Zhuzhuna@users.noreply.github.com>
2019-12-23 20:40:39 +0300

    Исправлено 1,2,3,5+сериализация. Исправить 4

--- App/MainWin.xaml.cs
+++ App/MainWin.xaml.cs
@@ -2,6 +2,12 @@
 using SyncLib.Seeker;
 using System.Windows;
 using System.Windows.Input;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization.Formatters.Soap;
+using System.Xml.Serialization;
+using System.Windows.Forms;
+using System.IO;
+using System;
 
 namespace App
 {
@@ -50,6 +56,7 @@ namespace App
                     x.Accept(logger);
                 });
 
+                log.Text += string.Format("Синхронезируем " + pair.Master + " и " + pair.Slave +"\n");
                 logger.Log();
             }
 
@@ -66,6 +73,7 @@ namespace App
                     x.Accept(logger);
                 });
 
+                log.Text += string.Format("Синхронезируем " + pair.Slave + " и " + pair.Master + "\n");
                 logger.Log();
             }
         }
@@ -83,7 +91,51 @@ namespace App
                     data.Items.RemoveAt(index + 1);
                 }
             }
-            else MessageBox.Show("Вы не выбрали, что хотите изменить");
+            else System.Windows.MessageBox.Show("Вы не выбрали, что хотите изменить");
+        }
+
+        private void RemoveData_Click(object sender, RoutedEventArgs e)
+        {
+            data.Items.Clear();
+        }
+
+        private void RemoveLog_Click(object sender, RoutedEventArgs e)
+        {
+            log.Clear();
+        }
+
+        private void Load_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            if (dlg.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                if (dlg.FileName == null)
+                    System.Windows.MessageBox.Show("Вы что-то ничего не выбрали");
+                else
+                {
+                    XmlSerializer xmlFormat = new XmlSerializer(typeof(string));
+
+                    using (Stream fStream = File.OpenRead(dlg.FileName))
+                    {
+                        string logger =
+                          (string)xmlFormat.Deserialize(fStream);
+                        log.Text += string.Format(logger);
+                    }
+                }
+            }
+        }
+
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+            XmlSerializer xmlFormat = new XmlSerializer(typeof(string));
+            string fileName = string.Format("result{0}", DateTime.Now.Date.Day);
+
+            using (Stream fStream = new FileStream(fileName + ".xml",
+              FileMode.Create, FileAccess.Write, FileShare.None))
+            {
+                xmlFormat.Serialize(fStream, log.Text);
+            }
+            System.Windows.Forms.MessageBox.Show("Сохранено в " + fileName);
         }
     }
 }
